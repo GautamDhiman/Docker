a926a8284c5ca5731750b7deef43ddb70ffac057
GautamDhiman <gautamCodesHere@gmail.com>
2023-05-15 17:01:50 +0530

    Demo Express App

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("<h1> Hi This is simple express app.</h1>")
+})
+const port = 80;
+
+app.listen(port, (port) => {
+    console.log("server running")
+})
