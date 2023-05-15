3196f230ac07db8b5d0a4df98acbfc25eda41a9f
GautamDhiman <gautamCodesHere@gmail.com>
2023-05-15 17:12:53 +0530

    pot changes

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("<h1> Hi This is simple express app.</h1>")
 })
-const port = 3000;
+const port = 80;
 
-app.listen(port, (port) => {
+app.listen(port, () => {
     console.log("server running")
 })
